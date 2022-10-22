f1a97522f566e81b96cc35e412c19f9fdd52d2cd
Boris D <boris.dob@gmail.com>
2022-10-21 20:15:54 -0700

    Initial Commit

---
+++ OCamlGraphics/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  OCamlGraphics
+//
+//  Created by Boris on 2022-10-21.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ OCamlGraphics/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  OCamlGraphics
+//
+//  Created by Boris on 2022-10-21.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
